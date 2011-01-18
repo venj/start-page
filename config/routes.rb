9b20ce5965ef070308611366bb8fed431e6ffab7
venj <ersaclarke@gmail.com>
2011-01-18 19:09:44 +0800

    disable user creation

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,9 @@ ActionController::Routing::Routes.draw do |map|
 
   map.logout '/logout', :controller => 'sessions', :action => 'destroy'
   map.login '/login', :controller => 'sessions', :action => 'new'
-  map.register '/register', :controller => 'users', :action => 'create'
-  map.signup '/signup', :controller => 'users', :action => 'new'
-  map.resources :users
+  #map.register '/register', :controller => 'users', :action => 'create'
+  #map.signup '/signup', :controller => 'users', :action => 'new'
+  map.resources :users, :except => [:new, :create]
 
   map.resource :session
   map.admin "/admin", :controller => :admin, :action => :index
